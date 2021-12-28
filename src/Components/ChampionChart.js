ef13ac5663b68f9ffd615e0ce6ab8b647c0e6ec1
Jamalfox85 <Fox.Jamal@outlook.com>
2021-12-28 00:57:46 -0500

    added almost all API GET functionality. Page loads champions and a modal with champion data including name, title, tags, blurb, and basic stats. Next commit will introduce advanced champion stats and styling

---
+++ src/Components/ChampionChart.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+const ChampionChart = ({ statData }) => {
+  const data = {
+    labels: ["Attack", "Defense", "Magic", "Difficulty"],
+    datasets: [
+      {
+        label: "Champion Stats",
+        data: statData,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Radar data={data} />
+    </div>
+  );
+};
+
+export default ChampionChart;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import AdvancedDetails from "./AdvancedDetails";
 import Filter from "./Filter";
 import styles from "./Main.module.css";
+import Modal from "react-modal";
+import ChampionChart from "./ChampionChart";
 
 const Main = () => {
   const [data, setData] = useState(null);
@@ -9,8 +10,8 @@ const Main = () => {
   const [loading, setLoading] = useState(true);
   const [allChampions, setAllChampions] = useState(true);
   const [filter, setFilter] = useState(null);
-  const [showDetails, setShowDetails] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalData, setModalData] = useState({});
   const tags = ["Assassin", "Fighter", "Mage", "Marksman", "Support", "Tank"];
 
   const displayAll = () => {
@@ -21,12 +22,10 @@ const Main = () => {
     setFilter(tags[id]);
   };
 
-  const handleShowDetails = () => {
-    setShowDetails(true);
-  };
-  const handleHideShowDetails = () => {
-    setShowDetails(false);
-  };
+  function toggleModal(modalId) {
+    setIsOpen(!isOpen);
+    console.log(modalData);
+  }
 
   useEffect(() => {
     fetch(
@@ -62,21 +61,52 @@ const Main = () => {
       return champion[1].tags.includes(filter);
     }
   });
-
+  console.log(data);
   return (
     <div className={styles.wrapper}>
       <h1>test</h1>
       <Filter displayAll={displayAll} displayFilter={displayFilter} />
       <div className={styles.championgrid}>
-        {championList.map(([key, value]) => (
+        {championList.map(([key, value, index]) => (
           <div className={styles.championcard} key={key}>
             {value.name}
             {value.title}
             {value.tags}
-            <button onClick={handleShowDetails}>Display Adv Details</button>
-            {showDetails ? <AdvancedDetails /> : null}
+            {/* <button onClick={() => toggleModal(key)}> */}
+            <button
+              onClick={() => {
+                toggleModal();
+                setModalData({
+                  name: value.name,
+                  title: value.title,
+                  tags: value.tags,
+                  blurb: value.blurb,
+                  stats: [
+                    value.info.attack,
+                    value.info.defense,
+                    value.info.magic,
+                    value.info.difficulty,
+                  ],
+                  // info: value.stats,
+                  info: Object.entries(value.stats),
+                });
+              }}
+            >
+              Display Adv Details
+            </button>
+            {/* {showDetails ? <AdvancedDetails/> : <h1>Test</h1>} */}
           </div>
         ))}
+        <Modal isOpen={isOpen} onRequestClose={toggleModal} ariaHideApp={false}>
+          <div>{modalData.name}</div>
+          <div>{modalData.title}</div>
+          <div>{modalData.tags}</div>
+          <hr />
+          <div>{modalData.blurb}</div>
+          <ChampionChart statData={modalData.stats} />
+          {/* {modalData.info.map(([key,item])=>(<div>{key}{item}</div>))} */}
+          <button onClick={toggleModal}>Close modal</button>
+        </Modal>
       </div>
     </div>
   );
